1cb1735f79cdfbfab9dff1068bd821b52b68d2d3
Mazen Megahed <mazen.megahed1@outlook.com>
2025-08-05 20:06:40 +0300

    Part2 _Q1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+
             #region Q1
             //1-a
             //2 - a
@@ -16,6 +17,48 @@
             //9 - b
             //10 - c
             #endregion
+
+            #region part 2 -Q1
+           ICircle circle = new Circle();
+            circle.displayshapeinfo();
+            IRectangle rectangle = new Rectangle();
+            rectangle.displayshapeinfo();
+            #endregion
+
+        }
+        interface IShape
+        {
+            public double Area { get; set; }
+            public void displayshapeinfo();
+
+        }
+        interface ICircle : IShape
+        {
+
+
+
+        }
+        interface IRectangle : IShape
+        {
+
+        }
+        class Circle : ICircle
+        {
+            public double Area { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void displayshapeinfo()
+            {
+                Console.WriteLine(  "This is Circle");            }
+        }
+        class Rectangle : IRectangle
+        {
+            public double Area { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+            public void displayshapeinfo()
+            {
+                Console.WriteLine(  "This is Rectangle");
+            }
         }
     }
+
 }
