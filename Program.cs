eb632ab23b2e57dba74802383d258a732f7b8f0a
Mazen Megahed <mazen.megahed1@outlook.com>
2025-08-05 19:46:59 +0300

    Question 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1-a
+            //2 - a
+            //3 - b
+            //4 - b
+            //5 - d
+            //6 - a
+            //7 - b
+            //8 - a
+            //9 - b
+            //10 - c
+            #endregion
         }
     }
 }
